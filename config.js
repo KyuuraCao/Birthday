ede840c3877de01b717f365be6d96d7405ff7c47
kyuuracao <vclancc@gmail.com>
2025-05-17 17:36:11 +0800

    first commit

---
+++ config.js
@@ -0,0 +1,30 @@
+var config = {
+  texts: [
+    "For You",
+    "My beloved cutie",
+    "Today is your birthday",
+    "Today is",
+    "Your 20th birthday",
+    "lorem",
+    "lorem",
+    "lorem",
+    "lorem",
+    "Ending",
+    "Probably gonna put my name at the end`",
+  ],
+
+  imgs: {
+    "My beloved cutie": "./imgs/princess.png",
+  },
+
+  desc: {
+    turn_on: "Turn On Lights",
+    play: "Play Music",
+    bannar_coming: "Let's Decorate",
+    balloons_flying: "Seems like something's missing",
+    cake_fadein: "Cake?",
+    light_candle: "Candle?",
+    wish_message: "Happy Birthday",
+    story: "A MESSAGE FOR YOU",
+  },
+};
